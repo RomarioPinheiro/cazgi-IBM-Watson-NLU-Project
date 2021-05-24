ae0f0a5dfa03d713388ed6e5bf2ca48d73472f05
RomarioPinheiro <pinheiroaraujo1991@gmail.com>
2021-05-24 19:08:04 +0000

    Server and client side changes to implement the final project requirements

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,5 +1,25 @@
 const express = require('express');
 const app = new express();
+const dotenv = require('dotenv');
+dotenv.config();
+
+function getNLUInstance() {
+    let api_key = process.env.API_KEY;
+    let api_url = process.env.API_URL;
+
+    const NaturalLanguageUndestandingV1 = require ('ibm-watson/natural-language-understanding/v1');
+    const {IamAuthenticator} = require ('ibm-watson/auth');
+
+    const naturalLanguageUnderstanding = new NaturalLanguageUndestandingV1({
+      version: '2021-03-25',
+      authenticator: new IamAuthenticator({
+       apikei: api_key,   
+      }),
+      serviceUrl: api_url,  
+    });
+    return naturalLanguageUnderstanding;
+
+}
 
 app.use(express.static('client'))
 
@@ -10,20 +30,20 @@ app.get("/",(req,res)=>{
     res.render('index.html');
   });
 
-app.get("/url/emotion", (req,res) => {
+app.get(getNLUInstance(), (req,res) => {
 
     return res.send({"happy":"90","sad":"10"});
 });
 
-app.get("/url/sentiment", (req,res) => {
+app.get(getNLUInstance(), (req,res) => {
     return res.send("url sentiment for "+req.query.url);
 });
 
-app.get("/text/emotion", (req,res) => {
+app.get(getNLUInstance(), (req,res) => {
     return res.send({"happy":"10","sad":"90"});
 });
 
-app.get("/text/sentiment", (req,res) => {
+app.get(getNLUInstance(), (req,res) => {
     return res.send("text sentiment for "+req.query.text);
 });
 
